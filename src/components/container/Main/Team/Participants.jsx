8aa4ba0728213936d99aa3f8b96a6ae36681bb7e
Lucas B <ajedrezalmafuerte@gmail.com>
2024-02-24 02:05:12 -0300

    Correct Team Modification

--- src/components/container/Main/Team/Participants.jsx
+++ src/components/container/Main/Team/Participants.jsx
@@ -13,13 +13,13 @@ const Participants = ({
   return (
     <div>
       <a
-        className="outline-none focus:after:content-[''] focus:after:block focus:after:border-b-2 focus:after:border-blue_title focus:after:mt-1 focus:after:w-1/2 focus:after:mx-auto"
+        className="relative outline-none focus:after:content-[''] focus:after:block focus:after:border-b-2 focus:after:border-blue_title focus:after:mt-1 focus:after:w-1/2 focus:after:mx-auto"
         href={href}
         title={title}
         target="_blank"
         rel="noopener noreferrer">
         <Bubble bg={color} />
-        <figure>
+        <figure className='absolute top-0 right-1'>
           <img className="max-w-48 h-48" src={imgSrc} loading="lazy" />
         </figure>
         <div className="mt-7">
