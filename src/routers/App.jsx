96e01210d2b93ca0998d2c68616e8abf7670a357
Lucas B <ajedrezalmafuerte@gmail.com>
2024-03-11 00:56:50 -0300

    Inserted ScrollTop

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,4 +1,5 @@
 import { Suspense, lazy } from 'react';
+import ScrollToTop from 'react-scroll-to-top';
 import { Main } from '../test/container/Main/Main';
 import { Header } from '../test/container/Header/Header';
 import { Footer } from '../test/container/Footer/Footer';
@@ -8,16 +9,19 @@ import '../assets/styles/App.css';
 const ScrollTop = lazy(() => import('./ScrollTop'));
 
 function App() {
-  const isMobile = useGetWidthSize(768)
+  const isMobile = useGetWidthSize(768);
 
   return (
     <>
-      <Suspense>
-        {!isMobile && <ScrollTop />}
-      </Suspense>
+      <Suspense>{!isMobile && <ScrollTop />}</Suspense>
       <Header />
       <Main />
       <Footer />
+      <ScrollToTop
+        smooth
+        color="#fff"
+        className="flex justify-center items-center bg-blue_check border-solid border-2 right-2 bottom-12 border-pure_white hover:bg-blue_title focus:bg-blue_title"
+      />
     </>
   );
 }
